ed82f580565828de03d8eb1464d15b9ca046e79b
Farjhan Ahmed <nkftt58346@gmail.com>
2025-05-03 11:39:48 +0600

    logging and exception added

---
+++ demo.py
@@ -0,0 +1,11 @@
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
+
+# # logging.debug('this is a debug message')
+
+# try:
+#     a = 1+'m'
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Farjhan Ahmed",
+    author_email="farjhanahmed@gmail.com",
+    packages=find_packages()
+)
